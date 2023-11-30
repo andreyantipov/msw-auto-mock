159f3d320d102011ea48a3007bafd9e34a43a235
Andrey Antipov <{ID}+{username}@users.noreply.github.com>
2023-11-30 16:26:37 +0000

    disable some tests

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,30 +1,30 @@
-import { promisify } from 'util';
-import { exec } from 'child_process';
-import { describe, expect, test } from 'vitest';
+// import { promisify } from 'util';
+// import { exec } from 'child_process';
+// import { describe, expect, test } from 'vitest';
 
-const escapedCwd = process.cwd().replace(/\\/g, '\\\\');
-const execAsync = promisify(exec);
-const timeout = 20000;
+// const escapedCwd = process.cwd().replace(/\\/g, '\\\\');
+// const execAsync = promisify(exec);
+// const timeout = 20000;
 
-expect.addSnapshotSerializer({
-  serialize(val, config, indentation, depth, refs, printer) {
-    return printer(val, { ...config, plugins: [] }, indentation, depth, refs)
-      .split(escapedCwd)
-      .join('.')
-      .replace(/\\\\/g, '/');
-  },
-  test(val) {
-    return val && Object.prototype.hasOwnProperty.call(val, 'startWorker');
-  },
-});
+// expect.addSnapshotSerializer({
+//   serialize(val, config, indentation, depth, refs, printer) {
+//     return printer(val, { ...config, plugins: [] }, indentation, depth, refs)
+//       .split(escapedCwd)
+//       .join('.')
+//       .replace(/\\\\/g, '/');
+//   },
+//   test(val) {
+//     return val && Object.prototype.hasOwnProperty.call(val, 'startWorker');
+//   },
+// });
 
-async function generateExample(name, args = '') {
-  const fixture = `./test/fixture/${name}.yaml`;
-  const output = `./example/test-${name}${args.replace(/\s+/g, '-')}.js`;
-  const command = `node ./dist/cli.js ${fixture} --output ${output} ${args}`;
-  await execAsync(command, { cwd: '.' });
-  return output;
-}
+// async function generateExample(name, args = '') {
+//   const fixture = `./test/fixture/${name}.yaml`;
+//   const output = `./example/test-${name}${args.replace(/\s+/g, '-')}.js`;
+//   const command = `node ./dist/cli.js ${fixture} --output ${output} ${args}`;
+//   await execAsync(command, { cwd: '.' });
+//   return output;
+// }
 
 // describe('example', () => {
 //   test.each([
